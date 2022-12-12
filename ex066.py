2ae123e319f305b3bef071039a8edd94e9c55987
Paulo Junior <103224304+Paulo-Jrfc@users.noreply.github.com>
2022-12-12 18:54:50 -0300

    Create ex066.py

---
+++ ex066.py
@@ -0,0 +1,8 @@
+soma = cont = 0
+while True:
+    núm = int(input('Digite um valor (999 para parar): '))
+    if núm == 999:
+        break
+    soma += núm
+    cont += 1
+print(f'A soma dos {cont} valores foi {soma}')
