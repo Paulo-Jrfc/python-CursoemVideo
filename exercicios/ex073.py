40c92cbcefb216bdec2f1e9caa094fbae2cd8e45
Paulo Junior <103224304+Paulo-Jrfc@users.noreply.github.com>
2022-12-13 11:47:54 -0300

    Create ex073.py

---
+++ exercicios/ex073.py
@@ -0,0 +1,17 @@
+times = ('Corinthians', 'Palmeiras', 'Santos', 'Grêmio',
+         'Cruzeiro', 'Flamengo', 'Vasco', 'Chapecoense',
+         'Atletico', 'Botafogo', 'Atlético-PR', 'Bahia',
+         'São Paulo', 'Fluminense', 'Sport Recife',
+         'EC Vitória', 'Coritiba', 'Avaí', 'Ponte Preta',
+         'Atltico-GO')
+print('-=' * 15)
+print(f'Lista de times {times}')
+print('-=' * 15)
+print(f'Os 5 primeiros são{times[:5]}')
+print('-=' * 15)
+print(f'Os ultimos 4 são {times[-4:]}')
+print('-=' * 15)
+print(f'Times em ordem alfabética: {sorted(times)}')
+print('-=' * 15)
+print(f'O Chapecoense está na {times.index("Chapecoense")+1} posição ')
+print('-=' * 15)
