7afca1383baacef38dc797ea68ee9a41f45e06e4
Paulo Junior <103224304+Paulo-Jrfc@users.noreply.github.com>
2022-12-15 15:49:43 -0300

    Create aula19a.py

---
+++ exemplos_aulas/aula19a.py
@@ -0,0 +1,8 @@
+pessoas = {'nome': 'Gustavo', 'sexo': 'M', 'idade': 29}
+print(f'O {pessoas["nome"]} tem {pessoas["idade"]} anos.')
+print('-' * 30)
+print(pessoas.keys())
+print('-' * 30)
+print(pessoas.values())
+print('-' * 30)
+print(pessoas.items())
