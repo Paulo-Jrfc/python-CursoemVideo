28a88512f511a761d2ae699d42f7b2f7c2976a86
Paulo Junior <103224304+Paulo-Jrfc@users.noreply.github.com>
2022-12-16 23:21:43 -0300

    Create aula20c.py

---
+++ exemplos_aulas/aula20c.py
@@ -0,0 +1,12 @@
+def soma(a, b):
+    print(f'A soma de {a} + {b} é igual a ', end='')
+    print(f'')
+    s = a + b
+    print(s)
+
+
+# Programa Principal
+soma(4, 5)
+soma(8, 9)
+soma(2, 1)
+soma(b=4, a=5)
