532854b4563fe88e88cb918079df89c672ca0b5b
Paulo Junior <103224304+Paulo-Jrfc@users.noreply.github.com>
2022-12-17 23:04:42 -0300

    Update sistema.py

--- exercicios/ex115/sistema.py
+++ exercicios/ex115/sistema.py
@@ -7,17 +7,22 @@ arq = 'cursoemvideo.txt'
 if not arquivoExiste(arq):
     criarArquivo(arq)
 
-
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoas', 'Sair do Sistema'])
     if resposta == 1:
         # Opção de listar o conteúdo de um arquivo!
         lerArquivo(arq)
     elif resposta == 2:
-        print('Opção 2')
+        # Opção de cadastrar uma nova pessoa
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
+        # Opção de sair do sistema
         print('Saindo do sistema... Até logo!')
         break
     else:
+        # Digitou uma opção errada no menu
         print('\033[31mERRO! Digite uma opção válida!\033[m')
-    sleep(2)
+    sleep(1.5)
